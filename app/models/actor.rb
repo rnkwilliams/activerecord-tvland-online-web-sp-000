c1228d1920f69660e44b76448dc1557082611cdf
Ranika Williams <rnkprice07@gmail.com>
2019-08-20 17:12:20 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name  #returns the first and last name of an actor
+    self.first_name + " " + self.last_name
+  end
+  
+  
+  #lists all of the characters that actor has alongside the show that the character is in
+  def list_roles 
+    characters.collect do |character|
+        "#{character.name} - #{character.show.name}"
+      end    
+  end
 end
